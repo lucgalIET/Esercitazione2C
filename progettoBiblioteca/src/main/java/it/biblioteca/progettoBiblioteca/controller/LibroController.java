47be261b6a983bc458b894a3f479fb6cd96f39ee
debscalo <debora.caloiero@gmail.com>
2023-07-03 13:00:40 +0200

    Update LibroController.java

--- progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/controller/LibroController.java
+++ progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/controller/LibroController.java
@@ -1,11 +1,16 @@
 package it.biblioteca.progettoBiblioteca.controller;
 
 
+import it.biblioteca.progettoBiblioteca.entities.LibroEntity;
+import it.biblioteca.progettoBiblioteca.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController

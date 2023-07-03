035db6d23f3a4457912a2f538f255430a9f2de8c
SimoneS2000 <simones143@gmail.com>
2023-07-03 11:47:46 +0200

    esercizio biblioteca

---
+++ progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/ProgettoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package it.biblioteca.progettoBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBibliotecaApplication.class, args);
+	}
+
+}

4179a427394d2da1abadd13b03abbd37036741cd
SimoneS2000 <simones143@gmail.com>
2023-07-03 13:15:18 +0200

    update

--- progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/repository/AutoreRepository.java
+++ progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/repository/AutoreRepository.java
@@ -2,8 +2,9 @@ package it.biblioteca.progettoBiblioteca.repository;
 
 import it.biblioteca.progettoBiblioteca.entities.AutoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface AutoreRepository extends JpaRepository<AutoreEntity, Long> {
 }

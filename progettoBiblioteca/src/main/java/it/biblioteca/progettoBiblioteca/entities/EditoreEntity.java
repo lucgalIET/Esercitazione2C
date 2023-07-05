d6504f5033ba901a1922ceccc3e1159600bbeec8
debscalo <debora.caloiero@gmail.com>
2023-07-05 09:29:18 +0200

    pom

---
+++ progettoBiblioteca/src/main/java/it/biblioteca/progettoBiblioteca/entities/EditoreEntity.java
@@ -0,0 +1,15 @@
+package it.biblioteca.progettoBiblioteca.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name= "autore")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EditoreEntity {
+}
